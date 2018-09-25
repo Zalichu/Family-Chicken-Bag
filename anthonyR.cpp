829182a219e663586db4e490498e42dacb25c529
Anthony Rodriguez <ZalsJourney@gmail.com>
2018-09-25 11:50:22 -0700

    credits

--- anthonyR.cpp
+++ anthonyR.cpp
@@ -1,11 +1,16 @@
-
 #include <iostream>
+#include "fonts.h"
 
 using namespace std;
 
-void printName()
+void printAnthony(int x, int y)
 {
-	
-	cout << "\nAnthony Rodriguez \n";
+	Rect c;
+	//glClearColor(0.1,0.1,0.1,1.0);
+
+	c.bot = y;
+	c.left = x;
+	c.center = 0;
+	ggprint8b(&c, 16, 0x00ff0000, "Anthony Rodriguez");
 }
 
