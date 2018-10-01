7df221667ddeb30d6b1e1605e44a7ce408d1631d
Anthony Rodriguez <ZalsJourney@gmail.com>
2018-09-30 20:25:47 -0700

    added a conveinent way to add color instead of using hex

--- anthonyR.cpp
+++ anthonyR.cpp
@@ -1,7 +1,17 @@
+//Author: Anthony Rodriguez
+
+//What this file includes:
+//	Credits
+// 	My Name
+//  Pause Menu
+//  Color System
+
 #include <iostream>
 #include "fonts.h"
 
 using namespace std;
+
+int color(string);
 
 void showAnthonyName(int x, int y)
 {
@@ -10,14 +20,14 @@ void showAnthonyName(int x, int y)
 	credit.bot = y + 50;
 	credit.left = x;
 	credit.center = 0;
-	ggprint8b(&credit, 16, 0x00ff0000, "CREDITS: ");
+	ggprint8b(&credit, 16, color("white"), "CREDITS: ");
 
 	//My Name
 	Rect c;
 	c.bot = y;
 	c.left = x;
 	c.center = 0;
-	ggprint8b(&c, 16, 0x00ff0000, "Anthony Rodriguez");
+	ggprint8b(&c, 16, color("white"), "Anthony Rodriguez");
 }
 
 
@@ -28,11 +38,45 @@ void titleScreen(int x, int y)
 	menu.bot = y;
 	menu.left = x;
 	menu.center = 0;
-	ggprint8b(&menu, 20, 0x00f000, "PLAY");
+	ggprint8b(&menu, 20, color("white"), "PLAY");
 
 	//Credits Button
 	menu.bot = y - 50;
 	menu.left = x;
 	menu.center = 0;
-	ggprint8b(&menu, 20, 0x00f000, "CREDITS");
+	ggprint8b(&menu, 20, color("white"), "CREDITS");
 }
+
+//Replaces the need to use hex codes
+int color(string colorChoice)
+{
+	if (colorChoice == "red") {
+		return 0xff0000;
+	}
+	if (colorChoice == "green") {
+		return 0xf000;
+	}
+	if (colorChoice == "yellow") {
+		return 0xffff33;
+	}
+	if (colorChoice == "blue") {
+		return 0x0000ff;
+	}
+	if (colorChoice == "purple") {
+		return 0xFF00FF;
+	}
+	if (colorChoice == "orange") {
+		return 0xFFBB52;
+	}
+	if (colorChoice == "black") {
+		return 0x000000;
+	}
+	if (colorChoice == "white") {
+		return 0xffffff;
+	}
+	return 0x0ff0000; //Default's to red
+}
+
+
+
+
