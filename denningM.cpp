3cf68a85ac608e83a09751869262ab72d928bf7e
MohammedBakraa <37638890+MohammedBakraa@users.noreply.github.com>
2018-10-02 12:13:35 -0700

    showDenningPicture

--- denningM.cpp
+++ denningM.cpp
@@ -3,6 +3,8 @@
 // Team 1
 #include <iostream>
 #include "fonts.h"
+#include <GL/glx.h>
+
 
 void showMasonName(int x, int y)
 {
@@ -14,4 +16,18 @@ void showMasonName(int x, int y)
     ggprint8b(&r, 16, 0x00ff000, "Mason Denning");
 }
 
-
+void showMasonPicture(int x, int y, GLuint texid)
+{
+    glColor3ub(255, 255, 255);
+    int wid = 40;
+    glPushMatrix();
+    glTranslatef(x, y, 0);
+    glBindTexture(GL_TEXTURE_2D, texid);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+    glEnd();
+    glPopMatrix();
+};
