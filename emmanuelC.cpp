99ce573573b9d5e157d23cdf20dc0c19d0482456
emmanuel1760 <42781142+emmanuel1760@users.noreply.github.com>
2018-10-08 10:14:10 -0700

    Update emmanuelC.cpp
    Fixed formatting and added comments.

--- emmanuelC.cpp
+++ emmanuelC.cpp
@@ -1,18 +1,22 @@
-//Emmanuel Chavez
+//Author: Emmanuel Chavez
+//Fall 2018
+//Purpose:
+//Shows author's name and image
+
 #include "fonts.h"
 #include <GL/glx.h>
 
-using namespace std;
-
+//prints author's name
 void showEmmanuelName(int x, int y)
 {	
-    Rect r;
-    unsigned int c = 0x00ffff44;
-    r.bot = y;
-    r.left = x;
-    r.center = 0;
-    ggprint8b(&r, 16, c, "Emmanuel Chavez");
+	Rect r;
+	unsigned int c = 0x00ffff44;
+	r.bot = y;
+	r.left = x;
+	r.center = 0;
+	ggprint8b(&r, 16, c, "Emmanuel Chavez");
 }
+//shows author's image
 void showEmmanuelPic(int x, int y, GLuint texid)
 {
 	glColor3ub(255,255,255);
